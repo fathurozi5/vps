4aeea7163f5ea485986f920ecc85809ea86dc564
fathurozi5 <56738680+fathurozi5@users.noreply.github.com>
2021-05-15 15:42:41 +0700

    Create DOGE.sh

---
+++ DOGE.sh
@@ -0,0 +1,8 @@
+#doge
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+
+tar xf cpuminer-opt-linux.tar.gz
+
+
+ ./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DD8gtV8Qg71TEWsL4knPu5FTr3xRkbWR6z.tuan3 -p c=DOGE
